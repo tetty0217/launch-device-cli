9eebf12078375d13a02bdac664a44985a4552e18
tetty0217 <dev.tetty@gmail.com>
2024-12-03 15:13:48 +0900

    chore: esm support

--- index.js
+++ index.js
@@ -1,106 +1,131 @@
-const { execSync, exec } = require("child_process");
-const { select } = require('@inquirer/prompts');
+#!/usr/bin/env node
+
+import { execSync } from "node:child_process";
+import { select } from "@inquirer/prompts";
 
 const runCommand = (command) => {
-  try {
-    return execSync(command, { encoding: "utf-8" }).trim();
-  } catch (error) {
-    console.error(`Error executing command: ${command}`);
-    process.exit(1);
-  }
+	try {
+		return execSync(command, { encoding: "utf-8" }).trim();
+	} catch (error) {
+		console.error(`Error executing command: ${command}`);
+		process.exit(1);
+	}
 };
 
 const getIOSRuntimes = () => {
-  const outputJson = runCommand("xcrun simctl list runtimes -j");
-  const output = JSON.parse(outputJson);
-
-  return output.runtimes;
+	const outputJson = runCommand("xcrun simctl list runtimes -j");
+	const output = JSON.parse(outputJson);
+	return output.runtimes;
 };
 
 const getIOSDevices = (runtime) => {
-  const outputJson = runCommand("xcrun simctl list devices -j");
-  const output = JSON.parse(outputJson);
-  
-  if (!(runtime in output.devices)) {
-    console.error(`Error not found runtime in output`);
-    process.exit(1);
-  }
+	const outputJson = runCommand("xcrun simctl list devices -j");
+	const output = JSON.parse(outputJson);
 
-  return output.devices[runtime];
+	if (!(runtime in output.devices)) {
+		console.error("Error not found runtime in output");
+		process.exit(1);
+	}
+
+	return output.devices[runtime];
 };
 
 const execIOSLaunch = async () => {
-  const runtimes = getIOSRuntimes();
-  const selectedRuntime = await select({
-    message: 'Select a runtime',
-    loop: true,
-    choices: runtimes.map((v) => ({
-      name: v.name,
-      value: v.identifier,
-    })),
-  });
+	const runtimes = getIOSRuntimes();
+	let selectedRuntime;
 
-  const devices = getIOSDevices(selectedRuntime);
-  const selectedDeviceId = await select({
-    message: 'Select a device type',
-    loop: true,
-    choices: devices.map((v) => ({
-      name: v.name,
-      value: v.udid,
-    })),
-  });
+	try {
+		selectedRuntime = await select({
+			message: "Select a runtime",
+			loop: true,
+			choices: runtimes.map((v) => ({
+				name: v.name,
+				value: v.identifier,
+			})),
+		});
+	} catch (error) {
+		console.log("Operation cancelled by user.");
+		process.exit(0);
+	}
 
-  console.log(`Booting device: ${selectedDeviceId} with ${selectedRuntime}...`);
-  runCommand(`xcrun simctl boot ${selectedDeviceId}`);
+	const devices = getIOSDevices(selectedRuntime);
+	let selectedDeviceId;
 
-  console.log("Opening Simulator app...");
-  runCommand("open -a Simulator");
+	try {
+		selectedDeviceId = await select({
+			message: "Select a device type",
+			loop: true,
+			choices: devices.map((v) => ({
+				name: v.name,
+				value: v.udid,
+			})),
+		});
+	} catch (error) {
+		console.log("Operation cancelled by user.");
+		process.exit(0);
+	}
 
-  console.log(`Simulator is ready: ${selectedDeviceId} with ${selectedRuntime}`);
-}
+	console.log(`Booting device: ${selectedDeviceId} with ${selectedRuntime}...`);
+	runCommand(`xcrun simctl boot ${selectedDeviceId}`);
+
+	console.log("Opening Simulator app...");
+	runCommand("open -a Simulator");
+};
 
 const getAndroidVirtualDevices = () => {
-  const output = runCommand("emulator -list-avds");
-
-  return output.split("\n").filter((v) => v);
-}
+	const output = runCommand("emulator -list-avds");
+	return output.split("\n").filter((v) => v);
+};
 
 const execAndroidLaunch = async () => {
-  const devices = getAndroidVirtualDevices();
-  const selectedAvd = await select({
-    message: 'Select a device',
-    loop: true,
-    choices: devices.map((v) => ({
-      value: v,
-    })),
-  });
+	const devices = getAndroidVirtualDevices();
+	let selectedAvd;
 
-  console.log(`Booting device: ${selectedAvd} ...`);
-  runCommand(`emulator -avd ${selectedAvd}`);
+	try {
+		selectedAvd = await select({
+			message: "Select a device",
+			loop: true,
+			choices: devices.map((v) => ({
+				value: v,
+			})),
+		});
+	} catch (error) {
+		console.log("Operation cancelled by user.");
+		process.exit(0);
+	}
 
-}
+	console.log(`Booting device: ${selectedAvd} ...`);
+	runCommand(`emulator -avd ${selectedAvd}`);
+};
 
 const main = async () => {
-  const osNames = ['iOS', 'Android'];
-  const selectedOSName = await select({
-    message: 'Select a OS',
-    loop: true,
-    choices: osNames.map((v) => ({
-      value: v,
-    })),
-  });
+	const osNames = ["iOS", "Android"];
+	let selectedOSName;
 
-  switch (selectedOSName) {
-    case 'iOS':
-      execIOSLaunch();
-      break;
-    case 'Android':
-      execAndroidLaunch();
-      break;
-    default:
-      console.error("Invalid OS name");
-      process.exit(1);
-  }
+	try {
+		selectedOSName = await select({
+			message: "Select a OS",
+			loop: true,
+			choices: osNames.map((v) => ({
+				value: v,
+			})),
+		});
+	} catch (error) {
+		console.log("Operation cancelled by user.");
+		process.exit(0);
+	}
+
+	switch (selectedOSName) {
+		case "iOS":
+			execIOSLaunch();
+			break;
+		case "Android":
+			execAndroidLaunch();
+			break;
+		default:
+			console.error("Invalid OS name");
+			process.exit(1);
+	}
 };
 
 main();
